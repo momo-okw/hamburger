f3a731487c0b8709c25d7e412a80292cad50b751
momo <ikmm0303@gmail.com>
2024-06-12 00:18:55 +0900

    js追加、sidebar編集

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $(".p-sidebar__title").on("click",function () {
+    $(".l-sidebar").toggleClass('active');
+    $(".p-sidebar__title").toggleClass('active');
+    });
+
+
+
+
+
+  });
