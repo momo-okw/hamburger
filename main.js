ced561e79a8a425d5fead82d736de9f315b7a5b0
momo <ikmm0303@gmail.com>
2024-06-16 17:06:47 +0900

    sidebar編集

--- main.js
+++ main.js
@@ -4,10 +4,21 @@ $(function() {
         if (window.matchMedia("(max-width: 960px)").matches) {
 
             $(".p-sidebar__title").on("click",function () {
-                $(".l-sidebar").toggleClass('active');
-                $(".p-sidebar__title").toggleClass('active');
+                $(".p-sidebar").addClass('active');
+                $(".p-sidebar__title").addClass('active');
+                $(".p-sidebar__batsu").addClass('active');
+                $(".p-sidebar__bg").addClass('active');
                 console.log("aa")
             });
+            $(".p-sidebar__batsu").on("click",function () {
+                $(".p-sidebar").removeClass('active');
+                $(".p-sidebar__title").removeClass('active');
+                $(".p-sidebar__batsu").removeClass('active');
+                $(".p-sidebar__bg").removeClass('active');
+                console.log("aa")
+            });
+
+
         } else {
             //何もしない
         
